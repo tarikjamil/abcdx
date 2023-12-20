1a1d7b6d92ca7b2364de20b1ceb6ffae21e14a7d
tarikjamil <tarikjamill@gmail.com>
2023-12-20 13:58:22 +0100

    edits

--- home.js
+++ home.js
@@ -11,3 +11,19 @@ $(".cta-link-wrapper").on("mouseenter mouseleave", function () {
     ease: "Quint.easeOut"
   });
 });
+
+<script type="text/javascript">
+piAId = '1051522';
+piCId = '21221';
+piHostname = 'go.skyhive.io';
+
+(function() {
+function async_load(){
+var s = document.createElement('script'); s.type = 'text/javascript';
+s.src = ('https:' == document.location.protocol ? 'https://' : 'http://') + piHostname + '/pd.js';
+var c = document.getElementsByTagName('script')[0]; c.parentNode.insertBefore(s, c);
+}
+if(window.attachEvent) { window.attachEvent('onload', async_load); }
+else { window.addEventListener('load', async_load, false); }
+})();
+</script>
